bcc3351a98f0534b6fb18fa8210875090424413b
Marcio Santos <marcioadsworks@gmail.com>
2024-10-28 21:05:02 -0300

    first commit

---
+++ src/main/java/com/example/core/commands/ReserveProductCommand.java
@@ -0,0 +1,17 @@
+package com.example.core.commands;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class ReserveProductCommand {
+
+    @TargetAggregateIdentifier
+    private final String productId;
+    private final Integer quantity;
+    private final String orderId;
+    private final String userId;
+
+}
